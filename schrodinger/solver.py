Corrected mathematics, provisionally removed energy calculation, updated some notebooks. TODO: open boundaries

schrodinger/solver.py
@@ -8,7 +8,7 @@ from . import wavefunction
 from . import utils
 
 
-def solve(psi0, t, x1, x2, V, BC, f=None, imag=False):
+def solve(psi0, t, x1, x2, V, BC, imag=False):
     """
     Solves the Schrödinger equation with time-dependent boundaries.
 
@@ -19,9 +19,7 @@ def solve(psi0, t, x1, x2, V, BC, f=None, imag=False):
         x2 (Callable[[float], float]): Function specifying the right boundary as a function of time.
         V (Callable[[float, np.ndarray[float]], np.ndarray[np.complex128]]): Potential energy function, 
             depends on time and spatial coordinates.
-        BC (str): Boundary condition, either 'periodic' or 'dirichlet'.
-        f (Optional[Callable[[float], Tuple[float, float]]]): Optional function specifying additional 
-            boundary dynamics. Default is None.
+        BC (str): Boundary condition, 'periodic', 'dirichlet' or 'pml'.
         imag (bool): Whether to perform the computation in imaginary time. Default is False.
 
     Returns:
@@ -32,55 +30,46 @@ def solve(psi0, t, x1, x2, V, BC, f=None, imag=False):
     Nt = len(t)
     Nx = len(psi0)
     psi = np.empty((Nt, Nx), dtype=np.complex128)
-    utils.check_bc(psi0, t[0], f)
+    utils.check_bc(psi0, BC)
     utils.check_bdry(t, x1, x2)
-    expT, up, up_t, up_xx = utils.boundaries(x1, x2, V, BC, f)
-
-    # Complexification of (t, x)
-    t += 0j
-    if imag:
-        t *= - 1j
+    N, dx, x, x_t, x_tt = utils.coords(x1, x2, Nx, BC)
     dt = np.concatenate(([0], np.diff(t)))
-    def x(t):
-        return np.linspace(x1(t), x2(t), Nx)+0j
+    if imag:
+        dt = -1j * dt
+
+   # Effective potential and momentum step
+    Veff = utils.compute_Veff(x, x_t, x_tt, x1, x2, V, BC)
+    expT = utils.compute_expT(x, x1, x2, N, BC)
 
     # Initial conditions
-    norm_2 = np.trapezoid(np.abs(psi0)**2, x(t[0]).real)
     psi[0, :] = psi0
-    uh = psi0 - up(t[0], x(t[0]))
-
-    # Inhomogeneous (boundary) terms
-    H_up = -1/2 * up_xx(t[0], x(t[0])) + V(t[0], x(t[0])) * up(t[0], x(t[0]))
-
-    # Energies
-    E, dE = [], []
-    E_i, dE_i = utils.energy(psi0, uh, H_up, x(t[0]), V(t[0], x(t[0])), norm_2, BC)
-    E.append(E_i)
-    dE.append(dE_i)
+    if BC == 'pml':
+        F = utils.stretch(x(t[0]), x1(t[0]), x2(t[0]))
+        scaling = np.exp(-1j * F * x_t(t[0]))
+        psi_i = utils.extrapolate(psi0, x(t[0]), x1(t[0]), x2(t[0]), Nx) * scaling
+    else:
+        scaling = np.exp(-1j * x(t[0]) * x_t(t[0]))
+        psi_i = psi0 * scaling
 
     # Integration loop
     for i in range(1, Nt):
-        # Inhomogeneous (boundary) terms, Euler step
-        H_up = -1/2 * up_xx(t[i], x(t[i])) + V(t[i], x(t[i])) * up(t[i], x(t[i]))
-        uh += -1j * dt[i] * (H_up - 1j*up_t(t[i], x(t[i])))
+        # Split operator step
+        psi_i *= np.exp(- .5j * dt[i] * Veff(t[i]))
+        psi_i  = expT(psi_i, t[i], dt[i], dx(t[i]))
+        psi_i *= np.exp(- .5j * dt[i] * Veff(t[i]))
 
-        # Homogeneous evolution, exponential integration
-        uh = uh * np.exp(- 1j * dt[i] * V(t[i], x(t[i])) / 2)
-        uh = expT(uh, dt[i], x(t[i]))
-        uh = uh * np.exp(- 1j * dt[i] * V(t[i], x(t[i])) / 2)
+        # Save data
+        if BC == 'pml':
+            F = utils.stretch(x(t[i]), x1(t[i]), x2(t[i]))
+            scaling = np.exp(-1j * F * x_t(t[i]))
+            psi[i, :] = (psi_i * scaling)[int(utils.D*Nx):-int(utils.D*Nx)]
+        else:
+            scaling = np.exp(1j * x(t[i]) * x_t(t[i]))
+            psi[i, :] = psi_i * scaling
 
-        # Normalisation
-        psi_i = uh + up(t[i], x(t[i]))
-        norm_2 = np.trapezoid(np.abs(psi_i)**2, x(t[i]).real)
-        psi[i, :] = psi_i
-
-        # Energies
-        E_i, dE_i = utils.energy(psi_i, uh, H_up, x(t[i]), V(t[i], x(t[i])), norm_2, BC)
-        E.append(E_i)
-        dE.append(dE_i)
-
-    if imag:
-        t *= 1j
-    t = t.real
-    sol = wavefunction.Wavefunction(psi, t, x1, x2, E, dE)
-    return sol
+    # Instruction to normalise in a closed domain
+    if BC == 'pml':
+        closed = False
+    else:
+        closed = True
+    return wavefunction.Wavefunction(psi, t, x1, x2, V, closed=closed)

schrodinger/utils.py
@@ -4,20 +4,37 @@ utils.py: Helper functions for solver.py.
 
 import numpy as np
 import scipy.fft as fft
+import finufft
+from scipy.interpolate import CubicSpline
 import jax
 import jax.numpy as jnp
 from jax import grad, vmap, jit
 jax.config.update('jax_enable_x64', True)
 
 
-def check_bc(psi0, t0, f, rtol=1e-5, atol=1e-8):
+# BOUNDARY CONDITION VALIDATION
+RTOL = 1e-5
+ATOL = 1e-8
+
+# PML PARAMETERS
+G  = jnp.pi / 4     # Absorption coefficient exp(1j*G)
+S0 = 0             # Absorption strength factor
+D  = .1            # Absorbing layer depth / solution domain
+
+# NOISE THRESHOLD (For derivatives)
+F = .99                     # Fraction of power spectrum we keep
+
+
+def check_bc(psi0, BC, rtol=RTOL, atol=ATOL):
     '''Checks whether boundary conditions are initially satisfied or not.'''
 
-    if f is not None:
-        f1, f2 = f
-        if not np.isclose(psi0[0], f1(t0), rtol, atol):
+    if BC == 'periodic':
+        if not jnp.isclose(psi0[0], psi0[-1], rtol, atol):
+            raise ValueError(f'Periodic bdry. cond. not satisfied with rtol={rtol:.2e}, atol={atol:.2e}.')
+    elif BC == 'dirichlet':
+        if not jnp.isclose(psi0[0], 0, rtol, atol):
             raise ValueError(f'Left bdry. cond. not satisfied with rtol={rtol:.2e}, atol={atol:.2e}.')
-        if not np.isclose(psi0[-1], f2(t0), rtol, atol):
+        if not jnp.isclose(psi0[-1], 0, rtol, atol):
             raise ValueError(f'Right bdry. cond. not satisfied with rtol={rtol:.2e}, atol={atol:.2e}.')
 
 
@@ -26,150 +43,158 @@ def check_bdry(t, x1, x2):
 
     L = x2(t) - x1(t)
     cond = L < 0
-    if np.any(cond):
-        i = np.argmax(cond)
+    if jnp.any(cond):
+        i = jnp.argmax(cond)
         raise ValueError(f'Boundary definition fails for t={t[i]:.2f}, negative domain length.')
 
 
-def boundaries(x1, x2, V, BC, f):
-    '''Computes helper functions based on the boundary conditions.'''
+def coords(x1, x2, Nx, BC):
+    '''Meshes for x and its derivatives.'''
+
+    def dx(t):
+        return (x2(t) - x1(t)) / (Nx - 1)
+    if BC == 'pml':
+        N = jnp.arange(-int(D*Nx), Nx + int(D*Nx))
+    else:
+        N = jnp.arange(Nx)
+    @jit
+    def x(t):
+        return x1(t) + dx(t) * N
+    @jit
+    def x_t(t):
+        x1_t = grad(x1)
+        dx_t = grad(dx)
+        return x1_t(t) + dx_t(t) * N
+    @jit
+    def x_tt(t):
+        x1_t = grad(x1)
+        x1_tt = grad(x1_t)
+        dx_t  = grad(dx)
+        dx_tt = grad(dx_t)
+        return x1_tt(t) + dx_tt(t) * N
+
+    return len(N), dx, x, x_t, x_tt
+
+
+def extrapolate(psi, x, x1, x2, Nx):
+    '''Smoothly tapers a wavefunction to zero in absorbing layers.'''
+
+    x0 = np.linspace(x1, x2, Nx)
+    x0 = np.concatenate(([x[0]], x0, [x[-1]]))
+    amplitude = np.abs(psi)
+    phase = np.angle(psi)
+    cs = CubicSpline(x0, np.concatenate(([0], amplitude, [0])), bc_type='natural')
+    amplitude_ext = cs(x)
+    idx = int(D*Nx)
+    phase_ext = np.concatenate(([phase[0]]*idx, phase, [phase[-1]]*idx))
+    psi_ext = amplitude_ext * np.exp(1j*phase_ext)
+
+    return psi_ext
+
+
+@jit
+def stretch(x, x1, x2):
+    '''Complex coordinate stretching involved in PML.'''
+
+    s1 = - (x1 - x)**3 * (x < x1)
+    s2 = (x - x2)**3 * (x > x2)
+    sigma = S0 * (s1 + s2)
+
+    return x + jnp.exp(1j * G) * sigma
+
+
+def compute_Veff(x, x_t, x_tt, x1, x2, V, BC):
+    '''Effective potential in moving domain.'''
+
+    if BC == 'pml':
+        @jit
+        def Veff(t):
+            F   = stretch
+            F_x = vmap(grad(F, holomorphic=True), (0, None, None))
+            F   = F(x(t), x1(t), x2(t))
+            F_x = F_x(x(t)+0j, x1(t), x2(t))
+            Vbc = F * x_tt(t) + (F_x - .5) * x_t(t)**2
+            return V(t, x(t)) + Vbc
+        return Veff
+    @jit
+    def Veff(t):
+        Vbc = x(t) * x_tt(t) + .5 * x_t(t)**2
+        return V(t, x(t)) + Vbc
+    return Veff
+        
+
+def compute_expT(x, x1, x2, Nx, BC):
+    '''Kinetic energy time step.'''
 
     if BC == 'periodic':
-        if f is not None:
-            raise ValueError('Periodic BCs do not support inhomogeneous terms.')
-        def up(t, x):
-            return 0j
-        expT = periodic
-    elif BC == 'dirichlet':
-        if f is not None:
-            def f1(t):
-                return f[0](t) + 0j
-            def f2(t):
-                return f[1](t) + 0j
-            f1_t = grad(f1, holomorphic=True)
-            f2_t = grad(f2, holomorphic=True)
-            @jit
-            def up(t, x):
-                M1 = jnp.array([
-                    [-1,             1],
-                    [3*x2(t), -3*x1(t)]
-                ]) / (6 * (x2(t) - x1(t)))
-                v1 = jnp.array([
-                    V(t, x1(t)) * f1(t) - 1j * f1_t(t),
-                    V(t, x2(t)) * f2(t) - 1j * f2_t(t)
-                ])
-                A, B = M1 @ v1
-                M2 = jnp.array([
-                    [-1,         1],
-                    [x2(t), -x1(t)]
-                ]) / (x2(t) - x1(t))
-                v2 = jnp.array([
-                    f1(t) - A*x1(t)**3 - B*x1(t)**2,
-                    f2(t) - A*x2(t)**3 - B*x2(t)**2
-                ])
-                C, D = M2 @ v2
-                return A*x**3 + B*x**2 + C*x + D
-        else:
-            def up(t, x):
-                return 0j
-        expT = dirichlet
-    else:
-        raise ValueError('Boundary conditions must be "periodic" or "dirichlet".')
-    @jit
-    def up_t(t, x):
-        func = grad(up, holomorphic=True, argnums=0)
-        return vmap(func, (None, 0))(t, x)
-    @jit
-    def up_xx(t, x):
-        func = grad(grad(up, holomorphic=True, argnums=1), holomorphic=True, argnums=1)
-        return vmap(func, (None, 0))(t, x)
-    return expT, up, up_t, up_xx
+        def expT(psi, t, dt, dx):
+            k = 2 * np.pi * fft.fftfreq(Nx, dx)
+            psi_f = fft.fft(psi)
+            psi_f = psi_f * np.exp(- .5j * dt * k**2)
+            psi = fft.ifft(psi_f)
+            return psi
+        return expT
+    if BC == 'dirichlet':
+        def expT(psi, t, dt, dx):
+            k = np.pi * np.arange(1, Nx + 1) / (Nx * dx)
+            psi_f = fft.dst(psi, type=1)
+            psi_f = psi_f * np.exp(- .5j * dt * k**2)
+            psi = fft.idst(psi_f, type=1)
+            return psi
+        return expT
+    if BC == 'pml':
+        @jit
+        def F(t):
+            return stretch(x(t), x1(t), x2(t))
+        @jit
+        def F_x(t):
+            F_x = vmap(grad(stretch, holomorphic=True), (0, None, None))
+            return F_x(x(t)+0j, x1(t), x2(t))
+        def expT(psi, t, dt, dx):
+            k = 2 * np.pi * fft.fftfreq(Nx, dx)
+            psi_f = fft.fft(F_x(t) * psi)
+            psi_f = psi_f * np.exp(- .5j * dt * k**2)
+            psi = fft.ifft(psi_f)
+            return psi
+        return expT
 
 
-def periodic(psi, dt, x):
-    '''Kinetic energy time step for periodic BC.'''
-
-    Nx = len(x)
-    dx = np.diff(x)[0]
-    k = 2 * np.pi * fft.fftfreq(Nx, dx)
-    psi_f = fft.fft(psi)
-    psi_f = psi_f * np.exp(- 1j * dt * k**2 / 2)
-    psi = fft.ifft(psi_f)
-    return psi
-
-
-def dirichlet(psi, dt, x):
-    '''Kinetic energy time step for dirichlet BC.'''
-
-    Nx = len(x)
-    dx = np.diff(x)[0]
-    k = np.pi * np.arange(1, Nx + 1) / (Nx * dx)
-    psi_f = fft.dst(psi, type=1)
-    psi_f = psi_f * np.exp(- 1j * dt * k**2 / 2)
-    psi = fft.idst(psi_f, type=1)
-    return psi
-
-
-def cutoff_periodic(wf_f, k, fraction=.999):
-    '''Noise threshold for periodic BC.'''
+def cutoff_periodic(wf_f, k, fraction=F):
+    '''Noise threshold.'''
 
     n = len(k)
 
     k_neg   = k[-1:n//2+n%2-1:-1]
     f_neg   = wf_f[-1:n//2+n%2-1:-1]
-    pow_neg = np.abs(f_neg)**2
-    cum_neg = np.cumsum(pow_neg) / np.sum(pow_neg)
-    i = np.argmax(cum_neg >= fraction)
+    pow_neg = jnp.abs(f_neg)**2
+    cum_neg = jnp.cumsum(pow_neg) / jnp.sum(pow_neg)
+    i = jnp.argmax(cum_neg >= fraction)
     kmin = k_neg[i]
 
     k_pos   = k[:n//2+n%2]
     f_pos   = wf_f[:n//2+n%2]
-    pow_pos = np.abs(f_pos)**2
-    cum_pos = np.cumsum(pow_pos) / np.sum(pow_pos)
-    i = np.argmax(cum_pos >= fraction)
+    pow_pos = jnp.abs(f_pos)**2
+    cum_pos = jnp.cumsum(pow_pos) / jnp.sum(pow_pos)
+    i = jnp.argmax(cum_pos >= fraction)
     kmax = k_pos[i]
 
     return kmin, kmax
 
 
-def cutoff_dirichlet(wf_f, k, fraction=.999):
+def cutoff_dirichlet(wf_f, k, fraction=F):
     '''Noise threshold for dirichlet BC.'''
 
-    power = np.abs(wf_f)**2
-    cumulative_power = np.cumsum(power) / np.sum(power)
-    i = np.argmax(cumulative_power >= fraction)
+    power = jnp.abs(wf_f)**2
+    cumulative_power = jnp.cumsum(power) / jnp.sum(power)
+    i = jnp.argmax(cumulative_power >= fraction)
     return k[i]
 
 
-def energy(psi, uh, H_up, x, V, norm_2, BC):
+def energy(psi, x, V):
     '''Average energy and standard deviation.'''
 
-    if BC == 'periodic':
-        def H(wf):
-            Nx = len(x)
-            dx = np.diff(x)[0]
-            k = 2 * np.pi * fft.fftfreq(Nx, dx)
-            wf_f = fft.fft(wf)
-            kmin, kmax = cutoff_periodic(wf_f, k)
-            k_filter = (k >= kmin) * (k <= kmax)
-            T_wf_f = k**2/2 * wf_f * k_filter
-            T_wf = fft.ifft(T_wf_f)
-            return T_wf + V * wf
-    elif BC == 'dirichlet':
-        def H(wf):
-            Nx = len(x)
-            dx = np.diff(x)[0]
-            k = np.pi * np.arange(1, Nx + 1) / (Nx * dx)
-            wf_f = fft.dst(wf, type=1)
-            kc = cutoff_dirichlet(wf_f, k)
-            k_filter = k <= kc
-            T_wf_f = k**2/2 * wf_f * k_filter
-            T_wf = fft.idst(T_wf_f, type=1)
-            return T_wf + V * wf
+    Nt, Nx = psi.shape
+    E = np.zeros(Nt)
+    dE = np.zeros(Nt)
 
-    H_psi = H(uh) + H_up
-    E = np.trapezoid(psi.conj()*H_psi, x).real / norm_2
-    H2_psi = H(H_psi)
-    E2 = np.trapezoid(psi.conj()*H2_psi, x).real / norm_2
-    dE = np.sqrt(max(0, E2 - E**2))
     return E, dE

schrodinger/visuals.py
@@ -20,10 +20,8 @@ matplotlib.rcParams.update({
 
 
 # 3D PLOT QUALITY
-# - Downsampling number of wavefunction datapoints by factor 1 / FACTOR
-# - Maximum grid points = MAXGRID
-FACTOR = 4
-MAXGRID = 100
+FACTOR = 4       # Downsampling number of wavefunction datapoints by factor 1 / FACTOR
+MAXGRID = 100    # Maximum grid points
 
 # WAVEFUNCTION COLOURMAP
 CMAP = 'hsv'
@@ -125,8 +123,11 @@ def animate_wf(wf):
     fig, ax = plt.subplots()
     ax.set_xlabel('x')
     ax.set_ylabel(r'$|\psi|$')
-    title = ax.text(.02, .95, '', bbox={'facecolor':'w', 'alpha':0.5, 'pad':5},
-                    transform=ax.transAxes, ha='left')
+    title = ax.text(
+        .02, .95, '',
+        bbox={'facecolor':'w', 'alpha':0.5, 'pad':5},
+        transform=ax.transAxes, ha='left'
+    )
     xmin = np.min(np.vectorize(wf.x1)(wf.t))
     xmax = np.max(np.vectorize(wf.x2)(wf.t))
     ymax = 1.1 * np.max(np.abs(wf.amplitude))
@@ -143,6 +144,9 @@ def animate_wf(wf):
         update_colored_line(lc, x, wf.amplitude[i])
         update_fill(left_shade, [xmin, x[0]], [0, 0], [ymax, ymax])
         update_fill(right_shade, [x[-1], xmax], [0, 0], [ymax, ymax])
-        title.set_text(rf't={wf.t[i]:.2f}, E={wf.E[i]:.2f}$\pm${wf.dE[i]:.2f}')
+        if wf.closed:
+            title.set_text(rf't={wf.t[i]:.2f}, E={wf.E[i]:.2f}$\pm${wf.dE[i]:.2f}')
+        else:
+            pass
         return lc, left_shade, right_shade, title,
     return animation.FuncAnimation(fig, fun, frames=Nt, interval=50, blit=True)

schrodinger/wavefunction.py
@@ -9,6 +9,7 @@ from jax import vmap
 import numpy as np
 
 from . import visuals
+from . import utils
 
 @dataclass
 class Wavefunction:
@@ -18,11 +19,15 @@ class Wavefunction:
     t:         np.ndarray
     x1:        Callable[[float], float]
     x2:        Callable[[float], float]
-    E:         np.ndarray
-    dE:        np.ndarray
+    V:         Callable[[float, np.ndarray], np.ndarray]
+    closed:    bool = True
 
     def __post_init__(self):
-        self._normalize()
+        x = self._x(self.t)
+        V = self.V(self.t, x)
+        if self.closed:
+            self._normalize()
+            self.E, self.dE = utils.energy(self.amplitude, x, V)
 
     def _x(self, t):
         Nx = self.amplitude.shape[1]
@@ -42,7 +47,10 @@ class Wavefunction:
                 raise ValueError(f"'t' value outside of range [{self.t[0]:.2f}, {self.t[-1]:.2f}]")
             i = np.argmin(np.abs(self.t - t))
             x = np.linspace(self.x1(self.t[i]), self.x2(self.t[i]), Nx)
-            title = rf't={self.t[i]:.2f}, E={self.E[i]:.2f}$\pm${self.dE[i]:.2f}'
+            if self.closed:
+                title = rf't={self.t[i]:.2f}, E={self.E[i]:.2f}$\pm${self.dE[i]:.2f}'
+            else:
+                pass
             visuals.plot(x, self.amplitude[i], xlabel='x', title=title)
         elif x is not None:
             x1, x2 = self.x1(self.t), self.x2(self.t)